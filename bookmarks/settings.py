5fc60731bbc1aaa89a7d3903b32e2eb021c37756
HanielDorton <danhorton0@gmail.com>
2013-06-05 14:41:13 -0700

    heroku2

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'links',                      # Or path to database file if using sqlite3.
+        'NAME':'d4bfnje2i98ir4',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'wholesale13',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'uapspgxihizvrk',
+        'PASSWORD': 'Py9u8EGGtQm1tKWm012YqONrRB',
+        'HOST': 'ec2-23-21-130-168.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
