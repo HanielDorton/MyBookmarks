563d65ad1145a0324bf93bd65ad9de09a0a54e93
HanielDorton <danhorton0@gmail.com>
2013-10-22 15:08:20 -0700

    updating notes

--- manage.py
+++ manage.py
@@ -4,10 +4,10 @@ import sys
 
 if __name__ == "__main__":
 	#This is for live settings:
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.settings")
+	#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.settings")
 	
 	#This is for test settings:
-	#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.TESTsettings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.TESTsettings")
 	
 	
 	from django.core.management import execute_from_command_line
