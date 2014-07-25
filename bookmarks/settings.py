1cc258fa9260f15f5a2d7fa8dfa51c4b9439d3a6
Haniel <haniel@haniel-HP-Pavilion-g6-Notebook-PC.(none)>
2014-07-24 18:20:14 -0700

    Updating Settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -67,6 +67,7 @@ STATIC_URL =  '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+ STATIC_PATH,
 	
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
